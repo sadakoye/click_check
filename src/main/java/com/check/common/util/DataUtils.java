55302e0b8729e4314bc4ed0f4d33d2a705d3db7d
zzc <709710914@qq.com>
2022-11-09 17:20:16 +0800

    2022/11/09

--- src/main/java/com/check/common/util/DataUtils.java
+++ src/main/java/com/check/common/util/DataUtils.java
@@ -1,10 +1,12 @@
 package com.check.common.util;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.check.system.pojo.vo.DictVo;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -28,6 +30,58 @@ public class DataUtils {
         BeanUtils.copyProperties(new PageInfo<>(list), tPageInfo);
         tPageInfo.setList(voList);
         return tPageInfo;
+    }
+
+    public static <T> QueryWrapper<T> baseQuery(Map<String, List<Object>> eqMap, Map<String, List<Object>> likeMap) {
+        QueryWrapper<T> queryWrapper = new QueryWrapper<>();
+        //精确匹配
+        if (eqMap != null && eqMap.size() > 0) {
+            for (String s : eqMap.keySet()) {
+                List<Object> objectList = eqMap.get(s);
+                if (objectList != null && objectList.size() > 0) {
+                    if (objectList.size() == 1) {
+                        queryWrapper.eq(s, objectList.get(0));
+                    } else {
+                        queryWrapper.and(tQueryWrapper -> {
+                            QueryWrapper<T> newQueryWrapper = new QueryWrapper<>();
+                            for (int i = 0; i < objectList.size(); i++) {
+                                Object o = objectList.get(i);
+                                newQueryWrapper.eq(s, o);
+                                if (i < objectList.size()) {
+                                    newQueryWrapper.or();
+                                }
+                            }
+                            return newQueryWrapper;
+                        });
+                    }
+                }
+            }
+        }
+        //模糊匹配
+        if (likeMap != null && likeMap.size() > 0) {
+            for (String s : likeMap.keySet()) {
+                List<Object> objectList = likeMap.get(s);
+                if (objectList != null && objectList.size() > 0) {
+                    if (objectList.size() == 1) {
+                        queryWrapper.like(s, "%" + objectList.get(0) + "%");
+                    } else {
+                        queryWrapper.and(tQueryWrapper -> {
+                            QueryWrapper<T> newQueryWrapper = new QueryWrapper<>();
+                            for (int i = 0; i < objectList.size(); i++) {
+                                Object o = objectList.get(i);
+                                newQueryWrapper.like(s, "%" + o + "%");
+                                if (i < objectList.size()) {
+                                    newQueryWrapper.or();
+                                }
+                            }
+                            return newQueryWrapper;
+                        });
+                    }
+                }
+            }
+        }
+
+        return queryWrapper;
     }
 
 }
