728c056b3e6ec60ac66d23b832d4ca62eb869025
zzc <709710914@qq.com>
2022-11-17 14:34:32 +0800

    2022/11/17

--- src/main/java/com/check/common/config/WebLogAspect.java
+++ src/main/java/com/check/common/config/WebLogAspect.java
@@ -2,14 +2,14 @@ package com.check.common.config;
 
 import com.alibaba.druid.util.StringUtils;
 import com.alibaba.fastjson.JSONObject;
+import com.check.security.pojo.bean.User;
+import com.check.security.utils.JWTUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -30,6 +30,9 @@ public class WebLogAspect {
     private final static int MAX_LENGTH = 1000;
     private final static String LINE_SEPARATOR = System.getProperty("line.separator");
     private final static String DOT = ".";
+
+    @Autowired
+    private JWTUtils jwtUtils;
 
     @Pointcut("execution(public * com.check..*.*Controller.*(..))")
     public void controllerMethodAop() {
@@ -52,16 +55,11 @@ public class WebLogAspect {
      * @param logBuffer logBuffer
      */
     private void appendUserLog(StringBuilder logBuffer) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        try {
-            if (authentication == null) {
-                logBuffer.append("+  USER: NO_LOGIN").append(LINE_SEPARATOR);
-            } else if ((!(authentication instanceof AnonymousAuthenticationToken)) && authentication.isAuthenticated()) {
-            } else {
-                logBuffer.append("+  USER: Anonymous").append(LINE_SEPARATOR);
-            }
-        } catch (Exception e) {
-            logBuffer.append("+  获取User失败！").append(LINE_SEPARATOR);
+        User user = jwtUtils.getUser();
+        if (user != null){
+            logBuffer.append("+  USER: ").append(user.getUsername()).append(LINE_SEPARATOR);
+        }else {
+            logBuffer.append("+  USER: NOT").append(LINE_SEPARATOR);
         }
     }
 
@@ -117,6 +115,7 @@ public class WebLogAspect {
         Long end = System.currentTimeMillis();
         Long costTime = end - begin;
         logBuffer.append("+  COST_TIME: ").append(costTime).append(LINE_SEPARATOR);
+        logBuffer.append("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++").append(LINE_SEPARATOR);
         return result;
     }
 }
